139e3c6c3cc694db25b796403a3f1e4fa884b7bd
Lucas Machado <lucasmachado.2018@outlook.com>
2026-02-26 23:20:59 -0300

    mais um teste ...

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
 // Rotas da API
-app.use(routes);
+app.use("/api", routes);
 
 // Rota teste (opcional)
 app.get("/", (req, res) => {
